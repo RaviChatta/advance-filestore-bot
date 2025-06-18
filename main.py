41f6f1418946b90b9c8a183d3910e9c5712d7a04
whoami847 <mrbeastbangla069@gmail.com>
2025-06-18 17:53:06 +0600

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ sys.path.append(os.path.abspath(os.path.dirname(__file__)))
 from bot import Bot
 import pyrogram.utils
 
-pyrogram.utils.MIN_CHANNEL_ID = -1002507596910
+# pyrogram.utils.MIN_CHANNEL_ID = -1002507596910 <-- এই লাইনটি মুছে ফেলা হয়েছে
 
 if __name__ == "__main__":
     Bot().run()
